0c4c696719e2137a38dcf9842e214a002ac5f201
Celia Gallay <celiagallay@gmail.com>
2023-10-21 21:46:51 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+})
+
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/register');
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+})
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/login');
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado en http://localhost:${port}`);
+});
